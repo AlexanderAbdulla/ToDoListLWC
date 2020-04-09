029782b1d165ff7e27aa646b3f1b193d302660aa
Alex Abdulla <alexander.amadeus.abdulla@gmail.com>
2020-04-09 13:06:20 -0700

    Edit ToDo Modal / Controller Completed

--- force-app/main/default/lwc/toDoComponent/toDoComponent.js
+++ force-app/main/default/lwc/toDoComponent/toDoComponent.js
@@ -1,10 +1,13 @@
 import { LightningElement, api, track } from 'lwc';
 import updateToDoStatus from '@salesforce/apex/toDoController.updateToDoStatus';
+import updateToDoNameDescription from '@salesforce/apex/toDoController.updateToDoNameDescription'
 
 export default class ToDoComponent extends LightningElement {
     @api todo;
     @track todo;
     @track value = '';
+    @track showModal = false;
+
 
     get options() {
         return [
@@ -31,5 +34,50 @@ export default class ToDoComponent extends LightningElement {
                 console.log(error);
         });
     }
+
+    openmodal() {
+        this.showModal = true
+    }
+
+    closeModal() {
+        this.showModal = false
+    }
+
+    saveMethod() {
+        console.log('save method invoked');
+        //console.log(this.template.querySelector("input[data-my-id=name-input]").value);
+        //console.log(this.template.querySelector("input[data-my-id=name-input]").placeholder);
+        var newName = this.template.querySelector("input[data-my-id=name-input]").placeholder;
+        var newDescription = this.template.querySelector("input[data-my-id=description-input]").placeholder;
+
+        console.log('passed variable dec');
+
+        if(this.template.querySelector("input[data-my-id=name-input]").value != "") {
+            newName = this.template.querySelector("input[data-my-id=name-input]").value;
+        } else {
+            console.log('in else');
+            newName = this.todo.Name;
+        }
+
+        if(this.template.querySelector("input[data-my-id=description-input]").value != "") {
+            newDescription = this.template.querySelector("input[data-my-id=description-input]").value;
+        } else {
+            console.log('in else 2');
+            newDescription = this.todo.Description__c;
+        }
+
+        updateToDoNameDescription({toDoId : this.todo.Id, name : newName, description : newDescription})
+        .then(result => {
+            console.log("success!!");
+            console.log(result);
+            this.todo = result;
+        })
+        .catch(error => {
+            console.log("failure!!!");
+            console.log(error);
+        });
+        this.closeModal();
+    }
+
 
 }
