6a83c3df93f18e102196882a54a4524603c467b7
Alex Abdulla <alexander.amadeus.abdulla@gmail.com>
2020-04-09 16:23:06 -0700

    Adding / Repopulating done

--- force-app/main/default/lwc/toDoContainer/toDoContainer.js
+++ force-app/main/default/lwc/toDoContainer/toDoContainer.js
@@ -1,5 +1,6 @@
 import { LightningElement, wire, track } from 'lwc';
 import getAllToDos from '@salesforce/apex/toDoController.getAllToDos';
+import createNewToDo from '@salesforce/apex/toDoController.createNewToDo';
 
 export default class ToDoContainer extends LightningElement {
     @track ToDos;
@@ -28,6 +29,31 @@ export default class ToDoContainer extends LightningElement {
     }
 
     saveCreateModal() {
+        console.log('save it!');
+        var newName = this.template.querySelector("input[data-my-id=name-input]").value;
+        var newDescription = this.template.querySelector("input[data-my-id=description-input]").value;
+        console.log(newName);
+        console.log(newDescription);
+
+        createNewToDo({name : newName, description : newDescription})
+        .then(result => {
+            console.log("success!!");
+            console.log(result);
+            // this.todo = result;
+            getAllToDos()
+            .then(result => {
+                console.log('successfully repopulated');
+                this.ToDos = result;
+            })
+            .catch(error => {
+                console.log('error in repopulating todos');
+                console.log(errror);
+            })
+        })
+        .catch(error => {
+            console.log("failure!!!");
+            console.log(error);
+        });
         this.showCreateModal = false
     }
 }
